19f6b5958b64761c973b8b51da3fe0d5af17800c
sequainoo <wazatoyung@gmail.com>
2021-12-02 19:01:37 +0000

    Add swap

--- monty.h
+++ monty.h
@@ -46,5 +46,6 @@ void push(stack_t **h, unsigned int line_number, const char *n);
 void pall(stack_t **h, unsigned int line_number);
 void pint(stack_t **h, unsigned int line_number);
 void pop(stack_t **h, unsigned int line_number);
+void swap(stack_t **h, unsigned int line_number);
 
 #endif

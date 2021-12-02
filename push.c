c8751c95d8ddffb64145e5a03ed81a82d32641cb
sequainoo <wazatoyung@gmail.com>
2021-12-02 18:37:14 +0000

    Add push

---
+++ push.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+
+/**
+ * is_number - iterates each character of string to check of isdigit
+ * @n: integer
+ * Return: 0 if is number, else -1 if not
+ */
+int is_number(const char *n)
+{
+    int i = 0;
+
+    if (*n == '-')
+        i = 1;
+    for (; *(n + i) != '\0'; i++)
+    {
+        if (isdigit(*(n + i)) == 0)
+            return (-1);
+    }
+    return (0);
+}
+
+/**
+ * push - adds node to the start of dlinkedlist
+ * @h: head of linked list (node at the bottom of stack)
+ * @line_number: bytecode line number
+ * @n: integer
+ */
+void push(stack_t **h, unsigned int line_number, const char *n)
+{
+    if (!h)
+        return;
+    if (is_number(n) == -1)
+    {
+        printf("L%u: usage: push integer\n", line_number);
+        free_dlist(h);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        if (add_end_node(h, atoi(n)) == -1)
+        {
+            free_dlist(h);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
